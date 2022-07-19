4417ecac072c2ad500dca82c4ac299c91fd9b386
john <johnjamero007@gmail.com>
2022-07-19 20:05:00 +0800

    adding sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  robotsTxtOptions: {
+     additionalSitemaps: [
+       'https://example.com/my-custom-sitemap-1.xml',
+       'https://example.com/my-custom-sitemap-2.xml',
+       'https://example.com/my-custom-sitemap-3.xml',
+     ],
+   },
+};
